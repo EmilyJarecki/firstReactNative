42335ebf6af66926fbcf880899e9fe5e9e206289
EmilyJarecki <jareckiemily@yahoo.com>
2023-08-13 16:04:48 -0500

    able to take screenshots and put them into medialibrary. can also do it on the web

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Platform } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
 import * as ImagePicker from "expo-image-picker"
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import CircleButton from './components/CircleButton';
 import IconButton from './components/IconButton';
 import EmojiPicker from './components/EmojiPicker';
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
 import {GestureHandlerRootView} from "react-native-gesture-handler"
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image'; // to capture screenshot via web
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 
 export default function App() {
+  const imageRef = useRef();
+
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
   const [showAppOptions, setShowAppOptions] = useState(false)
   const [pickedEmoji, setPickedEmoji] = useState(null);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true, // can crop
       quality: 1,
     })
+
+    // `requestPermission()` is a hook that allows access to media library
+    // `null` means it's neither granted nor denied
+    if (status === null) {
+      requestPermission();
+    }
 
     if (!result.canceled){
       setSelectedImage(result.assets[0].uri);
@@ -48,20 +60,50 @@ const onModalClose = () => {
 };
 
 const onSaveImageAsync = async () => {
-  // we will implement this later
+  if(Platform.OS !== 'web'){
+  try{
+    // `captureRef()` accepts optional argument where we can pass in size. Has many more in documentation
+    const localUri = await captureRef(imageRef, {
+      height: 440,
+      quality: 1,
+    })
+    await MediaLibrary.saveToLibraryAsync(localUri)
+    if (localUri){
+      alert("Saved!")
+    }
+  }catch(e){
+    console.log(e)
+  }} else {
+    try {
+      const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+        quality: 0.95,
+        width: 320,
+        height: 440,
+      });
+
+      let link = document.createElement('a');
+      link.download = 'sticker-smash.jpeg';
+      link.href = dataUrl;
+      link.click();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
 };
 
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer 
-        placeholderImageSource={PlaceholderImage} 
-        selectedImage={selectedImage}
-        />
-        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/> : null}
+        <View ref={imageRef} collapsable={false}>
+            <ImageViewer 
+            placeholderImageSource={PlaceholderImage} 
+            selectedImage={selectedImage}
+            />
+          {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/> : null}
+        </View>
       </View>
-
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
             <View style={styles.optionsRow}>
