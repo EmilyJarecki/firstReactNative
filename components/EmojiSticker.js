91eefff85bb3fc8ee95a41811739b5911687b3aa
EmilyJarecki <jareckiemily@yahoo.com>
2023-08-13 15:23:04 -0500

    able to drag emoji across image

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,5 +1,5 @@
 import {View, Image} from "react-native"
-import { TapGestureHandler } from 'react-native-gesture-handler';
+import { TapGestureHandler, PanGestureHandler } from 'react-native-gesture-handler';
 import Animated, {
     useAnimatedStyle,
     useSharedValue,
@@ -9,7 +9,10 @@ import Animated, {
 
 // the `createAnimatedComponent()` can wrap any component
 // determines which value is animated
+
+//THESE ARE BOTH COMPONENTS
 const AnimatedImage = Animated.createAnimatedComponent(Image); // this is to make double tap work 
+const AnimatedView = Animated.createAnimatedComponent(View);
 
 
 
@@ -20,6 +23,11 @@ const EmojiSticker = ({imageSize, stickerSource}) => {
     // shared value can be accessed and modified using the `.value` property which we will use in the `onDoubleTap()` function 
     const scaleImage = useSharedValue(imageSize); 
 
+    // translation values and need two seperate ones because it will be moving across the entire screen
+    // the `usedSharedValue()` hook decides initial position
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+
     // the `useAnimatedGestureHandler()` hook is used to animate the transition 
     const onDoubleTap = useAnimatedGestureHandler({
         onActive: () =>{
@@ -29,6 +37,24 @@ const EmojiSticker = ({imageSize, stickerSource}) => {
         }
     })
 
+    const onDrag = useAnimatedGestureHandler({
+        // when the gesture starts and its original position
+        onStart: (event, context) => {
+            // context will store the initial values of `translateX` and `translateY`
+            context.translateX = translateX.value;
+            context.translateY = translateY.value;
+        },
+
+        // when the gesture is moving
+        onActive: (event, context) => {
+            // `event` to get the current position of the pan gesture
+            // `context` to get the previously stored values of `translateX` and `translateY`
+            translateX.value = event.translationX + context.translateX;
+            translateY.value = event.translationY + context.translateY;
+          },
+    })
+
+
     // the `useAnimatedStyle()` hook updates styles when animation happens
     const imageStyle = useAnimatedStyle(()=> {
         return{
@@ -37,17 +63,33 @@ const EmojiSticker = ({imageSize, stickerSource}) => {
         }
     })
 
-  return (
-    <View style={{top: -350}}>
-        <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
-            <AnimatedImage 
-                source={stickerSource}
-                resizeMode="contain"
-                style={[imageStyle, {width: imageSize, height: imageSize}]}
-            />
-        </TapGestureHandler>
+    // the `useAnimatedStyle()` hook returns an array of transforms
+    const containerStyle = useAnimatedStyle(() => {
+        return {
+          transform: [
+            {
+              translateX: translateX.value,
+            },
+            {
+              translateY: translateY.value,
+            },
+          ],
+        };
+      });
+      
 
-    </View>
+  return (
+    <PanGestureHandler onGestureEvent={onDrag}>
+        <AnimatedView style={[containerStyle, {top: -350}]}>
+            <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+                <AnimatedImage 
+                    source={stickerSource}
+                    resizeMode="contain"
+                    style={[imageStyle, {width: imageSize, height: imageSize}]}
+                />
+            </TapGestureHandler>
+        </AnimatedView>
+    </PanGestureHandler>
   )
 }
 
